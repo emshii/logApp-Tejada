c43d4136a1afa82cef6c5e9d7b35bb2568e75ef0
emshii <201880223@psu.palawan.edu.ph>
2021-05-27 21:20:12 +0800

    done

---
+++ logApp-scaling-octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'http://localhost/phpsandbox/part5/logApp-scaling-octo/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', 'loveandme0426');
+	define('DB_NAME', 'logapp');
---
+++ logApp-scaling-octo/inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/emshii">emshii</a> for BSIT3-B1-A</p>
+    </div>
+</footer>
+  </body>
+</html>
